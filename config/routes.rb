2d59970ee9e01aeda0a32b3c00e822f4f48c0908
damiecode <codenlyn@gmail.com>
2020-08-04 12:51:32 +0100

    Add routes for login and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,15 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins '*'
-    devise_for :users
-    get 'savings/index'
-    root 'static#index'
-    namespace :api, defaults: {format: :json} do
-      namespace :vi do
-        devise_scope :user do
-          post "sign_up", to: "registrations#create"
-          post "logged_in", to: "sessions#create"
-        end
-        get 'category/get_categories'
-        resources :expenses, only: [:create, :index, :update]
+  devise_for :users
+  get 'savings/index'
+  root 'static#index'
+  namespace :api, defaults: {format: :json} do
+    namespace :vi do
+      devise_scope :user do
+        post "sign_up", to: "registrations#create"
+        post "logged_in", to: "sessions#create"
       end
+      get 'category/get_categories'
+      resources :expenses, only: [:create, :index, :update]
     end
   end
 end
