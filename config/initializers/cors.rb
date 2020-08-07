ba9d1ea98c2ea21dc1304482351f601f619ad6d2
damiecode <codenlyn@gmail.com>
2020-08-07 16:54:25 +0100

    Change CORS configuration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  url = Rails.env == 'production' ? 'https://trackingapp-api.herokuapp.com/' : 'http://localhost:3000'
-  allowed_methods = %i[get post put patch delete options head]
   allow do
-    origins url
-    resource '*', headers: :any, methods: allowed_methods, credentials: true
+    origins '*'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
